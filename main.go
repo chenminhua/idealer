d8efb2f97965abb810a606b0520a556fd36ce2d4
chenminhua <chenmh@blacklake.cn>
2018-04-23 19:51:42 +0800

    service ``

--- main.go
+++ main.go
@@ -6,22 +6,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var generator = &IDGenerator{}
+var service *IDService
 
 func handler(w http.ResponseWriter, request *http.Request) {
-	//category := mux.Vars(request)
-	value, _ := generator.GetNewId()
+	category := mux.Vars(request)["category"]
+	value, _ := service.GetNewId(category)
 	fmt.Fprintf(w, fmt.Sprintf("%d", value))
 }
 
 func main() {
-	generator.Init()
-
-	//for i := 0; i < 8; i++ {
-	//	fmt.Println(generator.GetNewId())
-	//}
+	InitDB()
+	service = CreateIdService()
 	router := mux.NewRouter()
-
 	router.HandleFunc("/v1/{category}", handler)
 	http.ListenAndServe(":8088", router)
 }
--- service.go
+++ service.go
@@ -1,54 +1,21 @@
 package main
 
-import "fmt"
-
-type IDGenerator struct{
-	AvailableIds []uint64
-	ch_proc chan chan uint64
+type IDService struct {
+	Generators map[string]*IDGenerator
 }
 
-// todo 反复重试
-func (gen *IDGenerator) GetNewIds(category string) {
-	bound := GetCurrentBound(category)
-	Acquire(category, bound, CAPACITY)
-	gen.AvailableIds = append(gen.AvailableIds, NewSlice(bound, CAPACITY, 1)...)
-	fmt.Println(gen.AvailableIds)
-}
-
-func (gen *IDGenerator) Init() {
-	// 连接Mysql
-	InitDB()
-	gen.GetNewIds(CATEGORY)
-
-	// 构建channel
-	gen.ch_proc = make(chan chan uint64, NUM_CHANNELS)
-	go gen.uuid_task()
-}
-
-func (gen *IDGenerator) GetNewId() (uint64, error) {
-	req := make(chan uint64, 1)
-	gen.ch_proc <- req
-	return <-req, nil
-}
-
-func (gen *IDGenerator) uuid_task() {
-	for {
-		ret := <-gen.ch_proc  //收到新请求
-		x := gen.AvailableIds[0]
-		gen.AvailableIds = gen.AvailableIds[1:]
-		ret <- x
-		if len(gen.AvailableIds) < CAPACITY * CAP_RATE {
-			gen.GetNewIds(CATEGORY)
-		}
+// todo 改成从数据库读配置
+func CreateIdService() *IDService {
+	materialLotGeneraotr := &IDGenerator{Category: "material_lot"}
+	materialLotGeneraotr.Init()
+	return &IDService{
+		Generators: map[string] *IDGenerator {
+			"material_lot": materialLotGeneraotr,
+		},
 	}
 }
 
-func NewSlice(start uint64, count, step uint64) []uint64 {
-	s := make([]uint64, count)
-	start += 1
-	for i := range s {
-		s[i] = start
-		start += step
-	}
-	return s
+func (idService *IDService) GetNewId(category string) (uint64, error) {
+	idGenerator := idService.Generators["material_lot"]
+	return idGenerator.GetNewId()
 }
