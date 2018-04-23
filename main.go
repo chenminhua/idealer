f76d55d132a374ff4bdf908a7d589cc30c6de482
chenminhua <chenmh@blacklake.cn>
2018-04-23 17:38:35 +0800

    add http endpoint

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+var generator = &IDGenerator{}
+
+func handler(w http.ResponseWriter, request *http.Request) {
+	//category := mux.Vars(request)
+	value, _ := generator.GetNewId()
+	fmt.Fprintf(w, fmt.Sprintf("%d", value))
+}
 
 func main() {
-	generator := &IDGenerator{}
 	generator.Init()
 
-	for i := 0; i < 8; i++ {
-		fmt.Println(generator.GetNewId())
-	}
+	//for i := 0; i < 8; i++ {
+	//	fmt.Println(generator.GetNewId())
+	//}
+	router := mux.NewRouter()
+
+	router.HandleFunc("/v1/{category}", handler)
+	http.ListenAndServe(":8088", router)
 }
 
 func checkErr(err error) {
